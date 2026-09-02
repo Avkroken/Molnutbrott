9fe712783ae4ecc2d2a7ea4b86c5dc3d5c71bcc2
Anders Eriksson <36226327+blixten85@users.noreply.github.com>
2026-09-02 18:37:08 +0200

    chore: allow future Terraform majors

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = "~> 1.15"
+  required_version = ">= 1.15"
 
   required_providers {
     cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 5.24.0"
+      source = "cloudflare/cloudflare"
     }
   }
 }
